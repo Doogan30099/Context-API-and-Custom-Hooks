ce66b852288d6d5b6e67d365a9120ebb6d9eba28
Patrick Nunes <pj.nunes.dy@gmail.com>
2025-10-21 11:22:22 -0400

    react login using usecontext

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,10 @@
+// src/context/UseContext.jsx
+
+import React from "react";
+
+const UserContext = React.createContext({
+  user: { name: "", isLoggedIn: false },
+  setUser: () => {}
+});
+
+export default UserContext;
